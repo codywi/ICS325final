19762719789e5bffbbbbe898450f136f634c8ce7
codywi <64983732+codywi@users.noreply.github.com>
2023-03-23 12:26:15 -0500

    testing service label

---
+++ project/php/manage_users.php
@@ -0,0 +1,37 @@
+<?php
+// variables to connect to the DB
+$servername = "";
+$username = "ics325sp230105";
+$password = "2944";
+// Short variables for the user variables
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+// Create the table if not exists
+// TODO: Figure out how to handle passwords and such
+$createTableIfNotExists = 'CREATE TABLE [IF NOT EXISTS] users(  
+    userID INT NOT NULL auto_increment Primary key,
+    userName varchar(40) NOT NULL,
+
+      )';
+function addUser($conn,$userID,$username)
+{
+    $sql = 'INSERT INTO users (userName) 
+    VALUES(' . $username.')';
+    if (mysqli_query($conn,$sql)) {
+        echo"record inserted sucessfully";
+    } else {
+     echo"Could not insert record: " .mysqli_error($conn);
+
+    }
+    mysqli_close($conn);
+}
+
+?>
