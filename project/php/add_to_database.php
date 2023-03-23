3297c227c3558d77350d130a29885904b3b15be9
codywi <64983732+codywi@users.noreply.github.com>
2023-03-23 12:00:01 -0500

    added add movie function

--- project/php/add_to_database.php
+++ project/php/add_to_database.php
@@ -11,15 +11,15 @@ $producer = "producer";
 $lead_actor = "lead_actor";
 // $services = "services"; NOT SURE HOW WE'RE GOING TO DO THIS ONE
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-// Create the table if
+// Create the table if not exists
 $createTableIfNotExists = 'CREATE TABLE [IF NOT EXISTS] movies(  
     movieID INT NOT NULL auto_increment Primary key,
     movie_title varchar (40) NOT NULL,  
@@ -28,14 +28,18 @@ $createTableIfNotExists = 'CREATE TABLE [IF NOT EXISTS] movies(
     producer varchar(40) NOT NULL,
     lead_actor varchar(40) NOT NULL
     -- services
-      ); '
-    ?>
+      )';
+function addMovie($conn,$movie_title, $year, $director, $producer, $lead_actor)
+{
+    $sql = 'INSERT INTO movies (movie_title,year,director,producer,lead_actor) 
+    VALUES(' . $movie_title . ',' . $year . ',' . $director . ',' . $producer . ',' . $lead_actor . ')';
+    if (mysqli_query($conn,$sql)) {
+        echo"record inserted sucessfully";
+    } else {
+     echo"Could not insert record: " .mysqli_error($conn);
 
+    }
+    mysqli_close($conn);
+}
 
-<!-- I goofed, here is a template for adding movies, I accidently did this first. 
-movieID INT NOT NULL auto_increment Primary key,'.
-    $movie_title
-    $year
-    $director
-    $producer
-    $lead_actor
+?>
