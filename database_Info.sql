eea91639890b0be92d30019a3a8a86fe7f432712
codywi <64983732+codywi@users.noreply.github.com>
2023-03-31 00:48:38 -0500

    added basic SQL structure we reviewed last Friday

---
+++ database_Info.sql
@@ -0,0 +1,41 @@
+create table if not exists 'customer'(
+    customerID int auto_increment not null, 
+    
+);
+
+create table if not exists 'movie_queue'(
+
+
+);
+
+create table if not exists 'movies'(
+ movieID int auto_increment not null,
+
+);
+
+create table if not exists 'actors'(
+actorID int auto_increment not null,
+
+);
+
+create table if not exists 'actor_movies'(
+
+
+);
+
+create table if not exists 'producers'(
+producerID int auto_increment not null,
+);
+
+create table if not exists'directors'(
+directorID int auto_increment not null
+
+);
+create table if not exists 'streaming_service'(
+ssID int auto_increment not null,
+
+);
+create table if not exists 'billing'(
+ billID int auto_increment not null,
+
+);
