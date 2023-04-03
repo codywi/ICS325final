e739af9ef6a3c0d97318568491340df772badbe1
codywi <64983732+codywi@users.noreply.github.com>
2023-04-03 16:53:31 -0500

    set up data in the create tables

--- database_Info.sql
+++ database_Info.sql
@@ -1,41 +1,55 @@
 create table if not exists 'customer'(
-    customerID int auto_increment not null, 
-    
+    customerID int auto_increment not null,
+    first_name varchar(40) not null, 
+    last_name varchar(40) not null,
+    email varchar(50) not null,
+    street_address varchar(20),
+    city varchar(30),
+    state varchar(2),
+    foreign key(customerID) references movie_queue(customerID),
 );
 
 create table if not exists 'movie_queue'(
-
-
+    queue_ID int not null,
+    movieID,
+    ssID,
+    primary key(customerID,movieID,ssID)
 );
 
 create table if not exists 'movies'(
- movieID int auto_increment not null,
+    movieID int auto_increment not null,
+    
 
 );
 
 create table if not exists 'actors'(
-actorID int auto_increment not null,
+    actorID int auto_increment not null,
 
 );
 
 create table if not exists 'actor_movies'(
+    actor_movies int NOT null,
 
+    Primary KEY (movieID,actorID)
 
 );
 
 create table if not exists 'producers'(
-producerID int auto_increment not null,
+    producerID int auto_increment not null,
+    p_first_name varchar not null,
+    p_last_name varchar not null,
 );
 
 create table if not exists'directors'(
-directorID int auto_increment not null
+    directorID int auto_increment not null
 
 );
 create table if not exists 'streaming_service'(
-ssID int auto_increment not null,
+    ssID int auto_increment not null,
 
 );
 create table if not exists 'billing'(
- billID int auto_increment not null,
+    billID int auto_increment not null,
+    customerID int 
 
 );
