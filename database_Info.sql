32f36706276b02cdd303cd702a07a63de743247b
codywi <64983732+codywi@users.noreply.github.com>
2023-04-06 15:36:44 -0500

    Session with prof

--- database_Info.sql
+++ database_Info.sql
@@ -8,15 +8,7 @@ create table customer(
     state varchar(2),
     userName varchar(20),
     password varchar(10),
-    foreign key(customerID) references movie_queue(customerID)
-);
-
-create table movie_queue(
-    queue_ID int not null,
-    movieID,
-    ssID,
-    primary key(customerID,movieID,ssID),
-    foreign key movieID references movies(movieID),
+    primary key(customerID)
 );
 
 create table movies(
@@ -26,21 +18,14 @@ create table movies(
     movie_director varchar(40) not null,
     movie_producer varchar(40) not null,
     movie_actor varchar(40) not null,
+    primary key(movieID)
 );
 
 create table actors(
     actorID int auto_increment not null,
     actor_first_name varchar(40) not null,
     actor_last_name varchar(40) not null,
-
-);
-
-create table actor_movies(
-    movieID int not null,
-    actorID int not null,
-    Primary KEY (movieID,actorID)
-    foreign key movieID references movies(movieID),
-    foreign key actorID references actors(actorID),
+    primary key(actorID)
 );
 
 create table producers(
@@ -52,8 +37,8 @@ create table producers(
 
 create table directors(
     directorID int auto_increment not null,
-    director_first_name varchar not null,
-    director_last_name varchar not null,
+    director_first_name varchar(40) not null,
+    director_last_name varchar(40) not null,
     primary key(directorID)
 );
 create table streaming_service(
@@ -62,9 +47,27 @@ create table streaming_service(
     service_notes varchar(40),
     primary key(ssID)
 );
+create table movie_queue(
+    customerID int not null,
+    movieID int not null,
+    ssID int not null,
+    primary key(customerID,movieID,ssID),
+    foreign key (customerID) references customer(customerID),
+    foreign key (movieID) references movies(movieID),
+    foreign key (ssID) references streaming_service(ssID)
+);
+
+create table actor_movies(
+    movieID int not null,
+    actorID int not null,
+    Primary KEY (movieID,actorID),
+    foreign key (movieID) references movies(movieID),
+    foreign key (actorID) references actors(actorID)
+);
 
 create table billing(
     billID int auto_increment not null,
     customerID int,
-    foreign key customerID references customer(customerID)
+    primary key(billID),
+    foreign key (customerID) references customer(customerID)
 );
