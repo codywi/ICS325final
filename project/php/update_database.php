65d56fc0eef942ea26014b930ad8666490792ac4
codywi <64983732+codywi@users.noreply.github.com>
2023-04-09 18:36:15 -0500

    Added update movie functionality

--- project/php/update_database.php
+++ project/php/update_database.php
@@ -1,33 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$db = mysqli_connect("localhost",'ics325sp230105','2944','ics325sp230105');
-// Short variables for the movie variables
-$title = "title";
-$year = "year";
-$director = "director";
-$producer = "producer";
-$actor = "lead_actor";
-// $selected_movie = "";
+<head>
+  <title> Movie Update</title>
+</head>
 
+<body>
+  <h1>Movie Update</h1>
 
-// check connection 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+  <?php
+
+  // Short variables for the movie variables
+  $title = (string) $_POST['title'];
+  $year = (int) $_POST['year'];
+  $director = (string) $_POST['director'];
+  $producer = (string) $_POST['producer'];
+  $actor = (string) $_POST['actor'];
+
+  $conn = mysqli_connect('localhost', 'ics325sp230105', '2944', 'ics325sp230105');
+  // check connection 
+  if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
   }
-  // 
+  echo nl2br("\nConnected successfully <br />");
 
-  echo  nl2br("\nConnected successfully");
+  $stmt = $conn->prepare("UPDATE movies SET title = ?, year = ? , director = ?, producer = ?, actor = ? WHERE title = ?");
 
-    $stmt = $conn->prepare("UPDATE movies (title, year, director, producer, actor) VALUES (?, ?, ?,?,?) WHERE ");
+  $stmt->bind_param("sissss", $title, $year, $director, $producer, $actor, $title);
 
-    $stmt->bind_param("sisss",$title, $year, $director, $producer, $actor);
-  
-    $stmt->execute();
+  $stmt->execute();
+  if ($stmt->error) {
+    echo "FAILURE!!! " . $stmt->error;
+  }
+  else echo "Updated {$stmt->affected_rows} rows";
 
 
-    echo "New records created successfully";
+  $stmt->close();
+  $conn->close();
 
-    $stmt->close();
-    $conn->close();
-    
-?>
+  ?>
+
+</body>
+
+</html>
