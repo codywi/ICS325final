1ab425ca89b36985c2c7145379a1c75d6479cf54
codywi <64983732+codywi@users.noreply.github.com>
2023-04-09 10:19:24 -0500

    added code to update DB, need to figure out how to select data to modify

--- project/php/update_database.php
+++ project/php/update_database.php
@@ -7,21 +7,27 @@ $year = "year";
 $director = "director";
 $producer = "producer";
 $lead_actor = "lead_actor";
+// $selected_movie = "";
 
 
+// check connection 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
+  // 
+
+  echo  nl2br("\nConnected successfully");
+
+    $stmt = $conn->prepare("UPDATE movies (movie_title, movie_year, movie_director, movie_producer, movie_actor) VALUES (?, ?, ?,?,?) WHERE ");
+
+    $stmt->bind_param("sisss",$movie_title, $year, $director, $producer, $lead_actor);
   
-  $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
-  
-  if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully";
-  } else {
-    echo "Error updating record: " . mysqli_error($conn);
-  }
-  
-  mysqli_close($db);
+    $stmt->execute();
 
 
+    echo "New records created successfully";
+
+    $stmt->close();
+    $conn->close();
+    
 ?>
